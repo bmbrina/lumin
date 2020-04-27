a8e319e8ea446255edafd2aebf9d5f2b9cdff1dc
Barbara M Brina <bmbrina9@gmail.com>
2020-04-26 22:02:14 -0500

    Updates

---
+++ precache-manifest.51a0e8a7137860563fb500ed0b0d51ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3be18e732f933ec6bdd5d9656bf185a",
+    "url": "/lumin/index.html"
+  },
+  {
+    "revision": "eeaa535b752067004e3d",
+    "url": "/lumin/static/css/main.71b3859c.chunk.css"
+  },
+  {
+    "revision": "c547434c4bf154d1e302",
+    "url": "/lumin/static/js/2.794559a1.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/lumin/static/js/2.794559a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeaa535b752067004e3d",
+    "url": "/lumin/static/js/main.b6c25f22.chunk.js"
+  },
+  {
+    "revision": "980aa683bb377d1cb411",
+    "url": "/lumin/static/js/runtime-main.b7fdc29a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lumin/precache-manifest.51a0e8a7137860563fb500ed0b0d51ea.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lumin/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
